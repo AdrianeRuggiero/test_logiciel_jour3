bcca4c0e0acffe4b349be64d6a698c750b99b5a4
AdrianeRuggiero <driketes@gmail.com>
2024-04-12 10:17:56 +0200

    second exercise

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-def sans_doublons(liste):
+"""def sans_doublons(liste):
     liste_sans_doublons = []
     for element in liste:
         if element not in liste_sans_doublons:
@@ -8,4 +8,17 @@ def sans_doublons(liste):
 # Exemple
 liste = [1, 2, 3, 4, 3, 2, 1]
 liste_final = sans_doublons(liste)
-print(liste_final)
+print(liste_final)"""
+
+
+def rotation_gauche(liste):
+    premier_element = liste[0]  # Stocke le premier élément
+    for i in range(len(liste) - 1): 
+        liste[i] = liste[i +1]  
+    liste[-1] = premier_element  
+    return liste
+
+# Exemple d'utilisation :
+liste = [1, 2, 3, 4]
+nouvelle_liste = rotation_gauche(liste)
+print(nouvelle_liste)
